f783add39c2acb62c92b6e7e1d69f46f2e026e9b
cecilemin <4.megabytes.disk@gmail.com>
2026-07-15 13:03:02 -0500

    initial commit from neocities build

---
+++ db.js
@@ -0,0 +1,126 @@
+const db        = document.getElementById("db");
+const BODY      = document.getElementById("dbBODY");
+const HEADER    = document.getElementById("dbHEADER");
+const fields    = [
+  "name",
+  "image",
+  "account",
+  "owner",
+  "typing"
+  ]; 
+  // IMPORTANT: the order of this array = the order each field is arranged in, left-to-right, when the database is intitialized.
+const template = {
+    "id"      : null,
+    "image"   : null,
+    "name"    : null,
+    "owner"   : null,
+    "account" : [null, null],
+    "typing"  : null
+};
+
+let registry  = new Object();
+registry["lemon"] = { // lemon's entry is hardcoded into the dictionary as a fall-back.
+    "id"      : "lemon",
+    "image"   : "lemon.jpg",
+    "name"    : "Lemon",
+    "owner"   : "PauIndeed",
+    "account" : ["@LEMONSYSEXE", "https://x.com/lemonsysexe"],
+    "typing"  : "🟡> example"
+  };
+
+
+const msg_link = "(Link to account)";
+const standard_image_size = "200px";
+const msg_db_INVALID_parameter = "INVALID DATABASE PARAMETER";
+const msg_db_INVALID_data = "INVALID DATA";
+
+function addEntry(id, image, name, owner, accountHandle, accountLink, typing) {
+  /*
+  cecilemin note:
+  this is a temporary way to add entries to the computerlings database dynamically. it is a clunky way to do it but hopefully we can build off of it for real i/o.
+  */
+  var entry = Object.create(template);
+  entry["id"] = id;
+  entry["image"] = image;
+  entry["name"] = name;
+  entry["owner"] = owner;
+  entry["account"] = [accountHandle, accountLink];
+  entry["typing"] = typing;
+  
+  registry[id] = entry;
+}
+
+function header() {
+  var LABEL;
+  for (var parameter of fields) {
+    LABEL           = document.createElement("td");
+    LABEL.innerHTML = parameter.toUpperCase();
+    dbHEADER.appendChild(LABEL);
+  }
+}
+function table() {
+  header();
+  for (var entry in registry) {
+    console.log(entry);
+    
+    var ROW   = document.createElement("tr");
+    ROW.id    = entry;
+    
+    for (var parameter of fields) {
+      //console.log(parameter);
+      //console.log(registry[entry][parameter]);
+      
+      var CELL    = document.createElement("td");
+      var ELEMENT;
+      var data    = registry[entry][parameter];
+      switch (parameter) {
+        case "image":
+          CELL.classList.add("photo");
+          ELEMENT           = document.createElement("img");
+          ELEMENT.src       = "/assets/photos/" + data;
+          ELEMENT.classList.add("photo");
+          break;
+        case "name":
+          CELL.classList.add("name");
+          ELEMENT           = document.createElement("p");
+          ELEMENT.innerHTML = data;
+          break;
+        case "owner":
+          CELL.classList.add("owner");
+          ELEMENT           = document.createElement("p");
+          ELEMENT.innerHTML = data;
+          break;
+        case "account":
+          CELL.classList.add("account");
+          ELEMENT           = document.createElement("a");
+          ELEMENT.href      = data[1];
+          ELEMENT.innerHTML = data[0];
+          break;
+        case "typing":
+          CELL.classList.add("typing");
+          ELEMENT           = document.createElement("p");
+          ELEMENT.innerHTML = data;
+          break;
+        default:
+          CELL.classList.add("INVALID");
+          ELEMENT           = document.createElement("p");
+          ELEMENT.innerHTML = msg_db_INVALID_parameter;
+          console.error("INVALID PARAMETER [" + parameter + "] IN ROW: " + ROW.id);
+          break;
+      }
+      CELL.appendChild(ELEMENT);
+      ROW.appendChild(CELL);
+    }
+    BODY.appendChild(ROW);
+  }
+}
+
+function initialize() { // we're calling this function the moment the html body loads.
+  // addEntry(id, image, name, owner, accountHandle, accountLink, typing)
+  addEntry("lime", "lime.jpg", "Lime", "PauIndeed", "@LEMONSYSEXE", "https://x.com/LEMONSYSEXE", "🟢> example");
+  addEntry("neroli", "neroli.jpg", "Neroli", "PauIndeed", "@LEMONSYSEXE", "https://x.com/LEMONSYSEXE", "🐟> example");
+  addEntry("stardust", "stardust.jpg", "Stardust", "PauIndeed", "@LEMONSYSEXE", "https://x.com/LEMONSYSEXE", "⭐️ example");
+  addEntry("lovedeath", "lovedeath.jpg", "Lovedeath", "PauIndeed", "@LEMONSYSEXE", "https://x.com/LEMONSYSEXE", "💜> example");
+  addEntry("flora", "flora.jpg", "Flora", "PauIndeed", "@LEMONSYSEXE", "https://x.com/LEMONSYSEXE", "🌻> example");
+  table();
+}
